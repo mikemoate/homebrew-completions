65245c21169f7a5594f7958c14f280cc5157a209
ilovezfs <ilovezfs@icloud.com>
2016-08-10 00:40:38 -0700

    wpcli-completion 0.24.1
    Closes #130.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- wpcli-completion.rb
+++ wpcli-completion.rb
@@ -1,8 +1,8 @@
 class WpcliCompletion < Formula
   desc "Bash completion for Wpcli"
   homepage "https://github.com/wp-cli/wp-cli"
-  url "https://github.com/wp-cli/wp-cli/archive/v0.24.0.tar.gz"
-  sha256 "43b1cefdfc7eb18433f369a1022123ff4d2d53eedf6670e9a29188735bbf7660"
+  url "https://github.com/wp-cli/wp-cli/archive/v0.24.1.tar.gz"
+  sha256 "df6dc42349fbe658c8c5a4fb2664b0eefe74a1fa4fba163f7db047fe4200c512"
 
   head "https://github.com/wp-cli/wp-cli.git"
 
